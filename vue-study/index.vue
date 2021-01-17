4e4ecb2790b4d81a6dfd950e7189b15306495700
luzenan <lzn8023>
2021-01-17 12:59:40 +0800

    first commit

---
+++ vue-study/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script>
+export default {};
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!"
+  }
+});
+// 定义一个名为 button-counter 的新组件
+Vue.component("button-counter", {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template:
+    '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
